33e158e0a7a2d8235d88dc5d00eafe5e443c4900
Amadeu Marques <maramadeu@gmail.com>
2025-09-26 00:00:37 +0100

    reduce channel

--- src/request_pre_processing/mod.rs
+++ src/request_pre_processing/mod.rs
@@ -30,7 +30,7 @@ mod worker;
 pub mod work_dividers;
 
 const ORCHESTRATOR_RCV_TIMEOUT: Option<Duration> = Some(Duration::from_micros(50));
-const PROPOSER_QUEUE_SIZE: usize = 8192;
+const PROPOSER_QUEUE_SIZE: usize = 2048;
 
 const RQ_PRE_PROCESSING_ORCHESTRATOR: &str = "RQ-PRE-PROCESSING-ORCHESTRATOR";
 
